ceb8986101a669912b4ba2387a80700a40300e94
ohsungj <7adam@naver.com>
2024-11-19 21:34:11 +0900

    Add files via upload

---
+++ java_study-1112/자바24111.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class 자바24111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("sss");
+		System.out.println("ddd");
+		System.out.println("aaa");
+	}
+
+}
---
+++ java_study-1112/자바241112.java
@@ -0,0 +1,55 @@
+package java_study;
+
+//자바 공불흘 시작하겠습니다 주석)
+public class 자바241112 {//자바는 한글지원이 가능
+
+	public static void main(String[] args) {//중괄호 시작
+		//여기에 코드를 작성예정
+		// TODO Auto-generated method stub
+System.out.println("안녕하세요,오늘 자바를 배웁니다");
+//ctrl+f11을 동시에 누르면 자바가 실행
+//system.out.println : 작성한 내용을 확인할 수 있는 기능
+System.out.println("hello world!");
+	System.out.println("ㅇㅁㅇ");
+	//syso >ctrl+space>enter(system.out.println 빠르게 타이핑
+	System.out.println("아무말"
+			);
+	System.out.println("대잔치");
+	System.out.println(  "ㅇㅅㅇ");
+	//코드 작성후 ctrl s 눌러서 저장 이클립스는 자동저장 아님
+	System.out.println(" 현재 년도는:"+2024+ "입니다");
+	//출력문에서 +는 문자를 더해줍니다
+	//system.out.println:  출력문
+	System.out.println(" 키:"+175+ "입니다");
+	System.out.println(" 나이:"+1+ "입니다");
+	System.out.print("안녕하세요");
+	System.out.print("안녕하세요");
+	//ln은 개행을 의미합니다(줄바꿈)
+	//ln이 없으면 가로로 쭉 출력됩니다
+	//변수 어떤값을 대입할때 사용
+	
+	
+	
+	//int 는 숫자를 의미 
+	//, String은 문자를 의미합니다 스트링 첫글자는 무조거ㅗㄴ
+	int age = 20; //나이
+	String adress = "대전 서구 둔산동"; //주소
+	String mbti="esfj";
+	
+    System.out.println("나이는: "+age);
+    System.out.println("주소는 : "+adress);
+    System.out.println("mbti:" + mbti);
+    
+   
+	System.out.println(" 키:"+175+ "입니다");
+	System.out.println(" 나이:"+1+ "입니다");
+	System.out.print("안녕하세요");
+	System.out.println(" 나이:"+1+ "입니다");
+	System.out.print("안녕하세요");
+	System.out.println(" 나이:"+1+ "입니다");
+    
+	}
+	
+	
+
+}
