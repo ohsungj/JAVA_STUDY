9427d8de260d3cbe3ef1bdc25991ca039971d9a6
ohsungj <7adam@naver.com>
2024-12-26 20:53:00 +0900

    Add files via upload

---
+++ java_study_1226/LibraryMain.java
@@ -0,0 +1,10 @@
+package java_study_1226;
+
+public class LibraryMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_study_1226/LibraryService.java
@@ -0,0 +1,5 @@
+package java_study_1226;
+
+public interface LibraryService {
+
+}
